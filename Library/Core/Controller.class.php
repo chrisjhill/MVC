1c14b4fe1e8c9c61162e6034f5ea21641cdc2478
Christopher Hill <cjhill@gmail.com>
2013-05-18 09:24:33 +0100

    Updated phpDoc.

--- Library/Core/Controller.class.php
+++ Library/Core/Controller.class.php
@@ -2,11 +2,15 @@
 namespace Core;
 
 /**
- * Connects the Controllers to the Views.
+ * Connects the Controller to the View.
  *
- * @copyright   2012 Christopher Hill <cjhill@gmail.com>
- * @author      Christopher Hill <cjhill@gmail.com>
- * @since       15/09/2012
+ * Handles common functionality between application controllers such as setting
+ * the layout, forwarding, and redirecting.
+ *
+ * @copyright Copyright (c) 2012-2013 Christopher Hill
+ * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @author    Christopher Hill <cjhill@gmail.com>
+ * @package   MVC
  */
 class Controller
 {
@@ -39,15 +43,22 @@ class Controller
 	/**
 	 * Change the layout from the default.
 	 *
-	 * To not use a layout pass in boolean false.
+	 * If you do not want your View Script to use a layout then pass in boolean
+	 * false. To set a new layout then pass in the layouts file name without
+	 * the .phtml extention. Your layouts are located in your MyProject/Layout
+	 * directory. For example:
+	 *
+	 * <code>
+	 * $this->view->setLayout('default');
+	 * </code>
 	 *
 	 * @access public
-	 * @param  mixed     $layout Which layout we wish to use.
+	 * @param  boolean|string $layout Which layout we wish to use.
 	 * @return boolean
-	 * @throws Exception         If the layout does not exist.
+	 * @throws Exception              If the layout does not exist.
 	 */
 	public function setLayout($layout) {
-		// Do we actually want a layout?
+		// If boolean false then do not output a layout
 		if ($layout === false) {
 			$this->view->layout = false;
 			return true;
@@ -55,42 +66,60 @@ class Controller
 
 		// The location of the layout
 		$templateUrlLayout = Config::get('path', 'base') . Config::get('path', 'project')
-			. 'layout/' . $layout . '.phtml';
+			. 'Layout/' . $layout . '.phtml';
 
-		// Does this layout exist?
+		// Check the layout actually exists before we set it
 		if (! file_exists($templateUrlLayout)) {
-			throw new \Exception('Layout does not exist.');
+			throw new \Exception("{$layout} layout does not exist.");
 		}
 
-		// The layout exists, so set it
+		// The layout exists
 		$this->view->layout = $layout;
 	}
 
 	/**
-	 * If the user wishes to forward the user to another controller.
+	 * Whilst in controller context, move to another controller/action.
 	 *
-	 * Since we no longer want to render this controller we set it as void. Then, when
-	 * the rendering is called it will be ignored.
+	 * When forwarding to a new action or controller/action, the URL will not
+	 * change, this is an internal redirect.
 	 *
-	 * This function will keep the same URL, that will not be changed between forwards.
+	 * Forwarding to an action in the same controller
+	 *
+	 * <code>
+	 * $this->forward('newAction');
+	 * </code>
+	 *
+	 * Forwarding to a new controller and action.
+	 *
+	 * <code>
+	 * $this->forward('newAction', 'newController');
+	 * </code>
 	 *
 	 * @access public
-	 * @param  string $action     The action we wish to forward to.
-	 * @param  string $controller The controller we wish to forward to.
-	 * @throws Exception          From the Router if the controller/action does not exist.
+	 * @param  string    $action     The action we wish to forward to.
+	 * @param  string    $controller The controller we wish to forward to.
+	 * @throws Exception             From the Dispatcher if the controller/action does not exist.
 	 */
 	public function forward($action = 'index', $controller = '') {
 		// Reregister the action in the profile
 		Profiler::deregister('Action', $this->view->action);
 
-		// Is this an controller forward or an action forward?
-		// Controller forward = A new controller
-		// Action redirect    = Same controller, different action
-		if ($controller == '' || $controller == str_replace(Config::get('settings', 'project') . '\\Controller\\', '', get_called_class())) {
+		// Set which controller has called this function. If it is the same as
+		// .. the user specified controller then we do not need to instantiate
+		// .. a whole new controller, we can simply forward to its action
+		$controllerCaller = str_replace(
+			Config::get('settings', 'project') . '\\Controller\\',
+			'',
+			get_called_class()
+		);
+
+		// Calling an action in the same controller
+		if ($controller == '' || $controller == $controllerCaller) {
 			Dispatcher::loadAction($this->child, $action);
-			$this->child->render();
-		} else {
-			// And start a new router to the desired controller/action
+		}
+
+		// Not the same controller, so dispatch to a new controller
+		else {
 			Profiler::deregister('Controller', $this->view->controller);
 			Dispatcher::loadController($controller, $action);
 		}
@@ -99,7 +128,8 @@ class Controller
 	/**
 	 * Redirect the user to a new page.
 	 *
-	 * This will perform a header redirect, so it will change the URL.
+	 * Unlike the forward() function, this will perform a header redirect,
+	 * causing the URL to change.
 	 *
 	 * @access public
 	 * @param  string $url The URL that we wish to redirect to.
